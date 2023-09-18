0e0bf381180f1de56f59ba3d10f0b3369c4087ee
Lomlomm <vanessaserna60@gmail.com>
2023-09-18 00:51:58 -0600

    Structure for waves request

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const API_URL = 'http://localhost:3001'; 
-const PATIENT_ID = '6c3056cc-7bc3-43cf-b707-7713cd9d93bb';
+const PATIENT_ID = '251ab25f-4c19-4372-8c1c-069fb48a6ac5';
+const TOKEN_AUTH = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNmMzMDU2Y2MtN2JjMy00M2NmLWI3MDctNzcxM2NkOWQ5M2JiIiwibmFtZSI6InZhbmVzc2EiLCJlbWFpbCI6InZhbmVAdmFuZWUuY29tIn0sImlhdCI6MTY5NTAxNDMwNCwiZXhwIjoxNjk1MTAwNzA0fQ.hB6vYhbk1YEeHWKqpyw3ghGKGACq8DeeBVXTZd26aHQ'; 
 
 module.exports = {
     API_URL,
     PATIENT_ID, 
+    TOKEN_AUTH,
 };
--- src/services/wavesService.tsx
+++ src/services/wavesService.tsx
@@ -1,9 +1,19 @@
 import axios from 'axios'
-import { API_URL, PATIENT_ID } from '../../config'
+import { API_URL, PATIENT_ID, TOKEN_AUTH } from '../../config'
 
 export const createStudy = async (file: File) => {
+    const request = {
+        headers: {
+            'Authorization': `Bearer ${TOKEN_AUTH}`,
+        },
+        data: file
+    };
+
+    console.log("token: ", request)
+
     try {
-        const response = await axios.post(`${API_URL}/api/studies/${PATIENT_ID}`, file);
+        const response = await axios.post(`${API_URL}/api/studies/${PATIENT_ID}`, request);
+        console.log(response)
         return response.data;
     } catch (error) {
         throw error;
