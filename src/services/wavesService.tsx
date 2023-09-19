56e0407202033c364aa8dd2a8c12e2e92914731a
Leonel Fernández <leonel-leonel-1@hotmail.com>
2023-09-18 20:10:13 -0600

    Upload csv fixed

--- src/services/wavesService.tsx
+++ src/services/wavesService.tsx
@@ -2,17 +2,15 @@ import axios from 'axios'
 import { API_URL, PATIENT_ID, TOKEN_AUTH } from '../../config'
 
 export const createStudy = async (file: File) => {
-    const request = {
-        headers: {
-            'Authorization': `Bearer ${TOKEN_AUTH}`,
-        },
-        data: file
-    };
-
-    console.log("token: ", request)
+    const formData = new FormData()
+    formData.append('csv', file);
 
     try {
-        const response = await axios.post(`${API_URL}/api/studies/${PATIENT_ID}`, request);
+        const response = await axios.post(`${API_URL}/api/studies/${PATIENT_ID}`, formData, {
+            headers: {
+                'Authorization': `Bearer ${TOKEN_AUTH}`,
+            }
+        });
         console.log(response)
         return response.data;
     } catch (error) {
