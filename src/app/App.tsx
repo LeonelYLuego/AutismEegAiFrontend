c7d503173110ebadb37015bff33ca07aa6e7fcb8
Lomlomm <vanessaserna60@gmail.com>
2023-09-28 11:10:16 -0600

    fixed briefly shown register page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,18 +9,16 @@ export default function App() {
   const apiUrl = "http://localhost:3001/api/users";
 
   useEffect(() => {
-    // Make an API request using Axios to check if the user exists
     axios
       .get(apiUrl)
       .then((response) => {
-        setUserExists(response.data === true);
-        localStorage.setItem("userExists", response.data === true);
+        setUserExists(response.data);
+        console.log(userExists);
       })
       .catch((error) => {
-        // If the API request fails or user doesn't exist
         setUserExists(false);
       });
-  }, []); // Empty dependency array means this effect will run once when the component mounts
+  }, []);
 
   return <main>{userExists ? <Login /> : <SignUp />}</main>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,29 @@ import Login from "@/pages/login/index";
 import SignUp from "@/pages/signup/index";
 
 export default function App() {
-  const [userExists, setUserExists] = useState(
-    localStorage.getItem("userExists") === "true"
-  );
+  const [userExists, setUserExists] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   const apiUrl = "http://localhost:3001/api/users";
 
   useEffect(() => {
     axios
       .get(apiUrl)
       .then((response) => {
-        setUserExists(response.data);
-        console.log(userExists);
+        const hasUsers = response.data.length > 0;
+        setUserExists(hasUsers);
       })
       .catch((error) => {
         setUserExists(false);
+      })
+      .finally(() => {
+        setIsLoading(false); // Update loading state when the request is complete
       });
+
   }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return <main>{userExists ? <Login /> : <SignUp />}</main>;
 }
