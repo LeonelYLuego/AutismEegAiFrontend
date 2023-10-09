1d2fbc4ebb18c1ff3f6e0dd51f580807e0ed5486
Pablo-Naranjo <pabnar1205@gmail.com>
2023-10-09 09:07:27 -0600

    Delete Studies

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const API_URL = 'http://localhost:3001'; 
 const PATIENT_ID = '450ce9ca-cd87-4905-a6d1-f86addeac0f7';
-const TOKEN_AUTH = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNTQ2ZGE1MWUtY2JiNS00ZWNiLWE1MjMtY2E5NjhhY2Y4Y2UyIiwibmFtZSI6IlBhYmxvIiwiZW1haWwiOiJwYWJsb0BwYWJsby5jb20ifSwiaWF0IjoxNjk2NTExNDExLCJleHAiOjE2OTY1OTc4MTF9.x_wxM6L1nW0lMEbGk9h5-oq6jTvZU1iEyvQbTZc-1qs'; 
+const TOKEN_AUTH = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiY2YxYzkyZTYtYjQyNS00ZmZlLTg1MmEtNTk5YzAxYTMxYmE0IiwibmFtZSI6InBhYmxvIiwiZW1haWwiOiJwYWJsb0BwYWJsby5jb20ifSwiaWF0IjoxNjk2ODI1NjE2LCJleHAiOjE2OTY5MTIwMTZ9.MPRPgDGqbQlesEOCbjjgOZPkyuOlAbHiHin1K4VWm_I'; 
                     
 module.exports = {
     API_URL,
--- src/pages/patientstudies/StudyList.tsx
+++ src/pages/patientstudies/StudyList.tsx
@@ -1,6 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { API_URL, TOKEN_AUTH } from "../../../config";
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Button,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "axios";
 
 interface Study {
@@ -11,6 +23,7 @@ interface Study {
 
 const StudyList: React.FC<{ patient_id: string }> = ({ patient_id }) => {
   const [studies, setStudies] = useState<Study[]>([]);
+  const [studyToDelete, setStudyToDelete] = useState<string | null>(null);
 
   useEffect(() => {
     const axiosInstance = axios.create({
@@ -37,6 +50,36 @@ const StudyList: React.FC<{ patient_id: string }> = ({ patient_id }) => {
       });
   }, [patient_id]);
 
+  const handleDeleteStudy = (studyId: string) => {
+    setStudyToDelete(studyId);
+  };
+
+  const confirmDeleteStudy = () => {
+    if (studyToDelete) {
+      const axiosInstance = axios.create({
+        baseURL: API_URL,
+        headers: {
+          Authorization: `Bearer ${TOKEN_AUTH}`,
+        },
+      });
+
+      axiosInstance
+        .delete(`/api/studies/${patient_id}/${studyToDelete}`)
+        .then((response) => {
+          // Handle successful deletion
+          // Update the state to remove the deleted study from the list
+          setStudies((prevStudies) =>
+            prevStudies.filter((study) => study.id !== studyToDelete)
+          );
+          setStudyToDelete(null);
+        })
+        .catch((error) => {
+          console.error("Error al eliminar el estudio:", error);
+          setStudyToDelete(null);
+        });
+    }
+  };
+
   return (
     <div>
       <Typography variant="h5">Lista de Estudios {patient_id}: </Typography>
@@ -48,6 +91,12 @@ const StudyList: React.FC<{ patient_id: string }> = ({ patient_id }) => {
               <ListItemText primary={`${study.id}`} />
               <ListItemText primary={`${study.created_on}`} />
               <ListItemText primary={`${study.result}`} />
+              <IconButton
+                color="secondary"
+                onClick={() => handleDeleteStudy(study.id)}
+              >
+                <DeleteIcon />
+              </IconButton>
             </ListItem>
           ))
         ) : (
@@ -56,6 +105,23 @@ const StudyList: React.FC<{ patient_id: string }> = ({ patient_id }) => {
           </Typography>
         )}
       </List>
+      {/* Confirmación de eliminación */}
+      <Dialog open={!!studyToDelete} onClose={() => setStudyToDelete(null)}>
+        <DialogTitle>Confirmar eliminación</DialogTitle>
+        <DialogContent>
+          <Typography>
+            ¿Estás seguro de que deseas eliminar este estudio?
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setStudyToDelete(null)} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={confirmDeleteStudy} color="primary">
+            Eliminar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
