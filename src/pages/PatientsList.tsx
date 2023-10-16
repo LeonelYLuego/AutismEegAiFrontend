618ef79d580c221f22522412253e8e1a68ae3a23
LAPTOP-90PAIAOG\josep <jose.padilla@kiubix.com>
2023-10-15 18:33:45 -0600

    Se agregó el botón para eliminar pacientes, incluyendo una ventana de confirmación.

--- src/pages/PatientsList.tsx
+++ src/pages/PatientsList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Button, List, ListItem, ListItemText } from "@mui/material";
+import { Button, List, ListItem, ListItemText, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@mui/material";
 import Header from "../components/Header";
+import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "axios";
 import { API_URL, TOKEN_AUTH } from "../../config";
 import router from "next/router";
@@ -13,14 +14,17 @@ interface Patient {
 
 const PatientsList = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
+  const [openConfirmationDialog, setOpenConfirmationDialog] = useState(false);
+  const [patientToDelete, setPatientToDelete] = useState("");
+
+  const axiosInstance = axios.create({
+    baseURL: API_URL,
+    headers: {
+      Authorization: TOKEN_AUTH,
+    },
+  });
 
   useEffect(() => {
-    const axiosInstance = axios.create({
-      baseURL: API_URL,
-      headers: {
-        Authorization: TOKEN_AUTH,
-      },
-    });
 
     axiosInstance
       .get("/api/patients")
@@ -39,11 +43,36 @@ const PatientsList = () => {
       });
   }, []);
 
+  const openDeleteConfirmation = (patientId: string) => {
+    setPatientToDelete(patientId);
+    setOpenConfirmationDialog(true);
+  };
+
+  const closeDeleteConfirmation = () => {
+    setOpenConfirmationDialog(false);
+  };
+
   const handlePatientClick = (patientId: string) => {
-    console.log(patientId);
     router.push(`/patientstudies/${patientId}`);
   };
 
+  const handleDeleteClick = (patientId: string) => {
+    axiosInstance
+      .delete(`/api/patients/${patientId}`)
+      .then((response) => {
+        if (response.status === 200) {
+          const updatedPatients = patients.filter((patient) => patient.id !== patientId);
+          setPatients(updatedPatients);
+        } else {
+          console.error("Error al eliminar el paciente:", response);
+        }
+      })
+      .catch((error) => {
+        console.error("Error al eliminar el paciente:", error);
+      });  
+    closeDeleteConfirmation();
+  };
+  
   return (
     <div>
       <Header />
@@ -60,10 +89,38 @@ const PatientsList = () => {
               >
                 Ver estudios
               </Button>
+              <IconButton
+                edge="end"
+                onClick={() => openDeleteConfirmation(patient.id)}
+              >
+                <DeleteIcon />
+              </IconButton>
             </ListItem>
           ))}
         </List>
       </div>
+      <Dialog open={openConfirmationDialog} onClose={closeDeleteConfirmation}>
+        <DialogTitle>Confirmación de eliminación</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            ¿Estás seguro de que deseas eliminar a este paciente?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDeleteConfirmation} color="primary">
+            Cancelar
+          </Button>
+          <Button
+            onClick={() => {
+              handleDeleteClick(patientToDelete);
+              closeDeleteConfirmation();
+            }}
+            color="primary"
+          >
+            Eliminar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
